84ed8c40f7fe202c27336aad72e90b52868cdd5b
Alexandr Kolupaev <theonekeyg@gmail.com>
2025-09-27 19:56:58 +0300

    More preferences

--- init.lua
+++ init.lua
@@ -176,6 +176,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Save current file
+vim.keymap.set('n', '<leader>w', '<cmd>w<CR>', { desc = 'Save current file' })
+
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
 -- is not what someone will guess without a bit more experience.
--- lua/custom/plugins/personalized.lua
+++ lua/custom/plugins/personalized.lua
@@ -1,3 +1,10 @@
 vim.o.tabstop = 4
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
+
+-- " Enable pasting from global clipboard
+-- " with CTRL+V in Insert mode
+vim.keymap.set('i', '<C-V>', '<Esc>"+pa', { desc = 'Paste "+ buffer' })
+
+-- Add empty line without exiting normal mode
+vim.keymap.set('n', '<leader>o', 'o<ESC>')
